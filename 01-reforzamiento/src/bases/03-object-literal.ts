ce250948a51e1945614fcfb2426f660a33176f32
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-09 15:13:39 -0600

    Interfaces de TypeScript.

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,6 +1,21 @@
-const ironman = {
+// Es un molde de cómo se quiere que luzca algo.
+// Es diferente a una clase.
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+    address: Address;
+};
+
+interface Address {
+    postalCode: string;
+    city: string;
+}
+
+const ironman: Person = {
     firstName: 'Tony',
     lastName: 'Stark',
+    // age: '45', // Indica error.
     age: 45,
     address: {
         postalCode: 'ABC123',
@@ -8,16 +23,28 @@ const ironman = {
     }
 };
 
-// spread operator
-// const spiderman = {...ironman};
-const spiderman = structuredClone(ironman);
+// const spiderman: Person = {
+//     firstName: "Peter",
+//     lastName: "Parker",
+//     age: 0,
+//     address: {
+//         postalCode: "",
+//         city: ""
+//     }
+// }
 
-spiderman.firstName = 'Peter';
-spiderman.lastName = 'Parker';
-spiderman.age = 22;
-spiderman.address.city = 'San José';
+console.log(ironman);
 
-console.log(ironman, spiderman);
+// // spread operator
+// // const spiderman = {...ironman};
+// const spiderman = structuredClone(ironman);
+
+// spiderman.firstName = 'Peter';
+// spiderman.lastName = 'Parker';
+// spiderman.age = 22;
+// spiderman.address.city = 'San José';
+
+// console.log(ironman, spiderman);
 
 
 

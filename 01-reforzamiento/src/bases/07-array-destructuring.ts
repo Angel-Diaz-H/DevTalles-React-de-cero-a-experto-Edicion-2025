b69a671d693af3e077fa407257408073edac5920
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-09 17:36:10 -0600

    Tarea - Desestructuración

--- 01-reforzamiento/src/bases/07-array-destructuring.ts
+++ 01-reforzamiento/src/bases/07-array-destructuring.ts
@@ -19,3 +19,15 @@ const returnsArrayFn = () => {
 const[letters, numbers] = returnsArrayFn();
 
 console.log(letters, numbers);
+
+// Tarea.
+const useState = (value: string) => {
+    return[value, (newValue: string) => {
+        console.log(newValue);
+    }] as const; // El segundo objeto lo tomará como función, no como string.
+};
+
+
+const [name, setName] = useState('Goku');
+console.log(name);       // Goku
+setName('Vegeta');       // Imprime "Vegeta"

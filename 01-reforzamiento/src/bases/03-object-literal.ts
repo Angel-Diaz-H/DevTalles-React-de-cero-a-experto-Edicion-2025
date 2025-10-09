f44fde70eaa581df2b73563e4c71fc53db9597e7
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-09 15:03:02 -0600

    Objetos literales

---
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -0,0 +1,23 @@
+const ironman = {
+    firstName: 'Tony',
+    lastName: 'Stark',
+    age: 45,
+    address: {
+        postalCode: 'ABC123',
+        city: 'New York'
+    }
+};
+
+// spread operator
+// const spiderman = {...ironman};
+const spiderman = structuredClone(ironman);
+
+spiderman.firstName = 'Peter';
+spiderman.lastName = 'Parker';
+spiderman.age = 22;
+spiderman.address.city = 'San José';
+
+console.log(ironman, spiderman);
+
+
+

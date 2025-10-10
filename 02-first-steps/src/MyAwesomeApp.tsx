d35988f0d6ee93093751b8d78386fb65458d696c
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-10 09:32:09 -0600

    Impresión de variables.

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,30 @@
-export function MyAwesomeApp () {
-    return(
+export function MyAwesomeApp() {
+    const firstName = 'Angel';
+    const lastName = 'Díaz';
+
+    const favoriteGame = ['Elden Ring', 'Smash', 'Metal Gear']
+    const isActive = true;
+
+    const address = {
+        zipCode: 'ABC-123',
+        country: 'Canadá'
+    };
+
+    return (
         <>
-            <h1>Angel</h1>
-            <h3>Díaz</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+
+            <p>{favoriteGame.join(', ')}</p>
+            <p>2+2 = {2 + 2}</p>
+
+            {/* No existe una representación visual de React para boolean. */}
+            <h1>{isActive}</h1>
+            <h1>{isActive ? 'Sí' : 'No'}</h1>
+
+            <p>
+                {JSON.stringify(address)}
+            </p>
         </>
     )
 };

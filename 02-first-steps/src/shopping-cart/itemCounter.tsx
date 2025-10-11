6e665f242a2140ca466611685b5f642bfc6464ba
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-11 15:55:33 -0600

    Archivos de CSS.

--- 02-first-steps/src/shopping-cart/itemCounter.tsx
+++ 02-first-steps/src/shopping-cart/itemCounter.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import './ItemCounter.css'
+
 interface Props {
     name: string;
     quantity?: number; //? para opcional
@@ -5,23 +8,30 @@ interface Props {
 };
 
 // Desestructuración
-export const ItemCounter = ({ name, quantity }: Props) => {
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
+    // Los hooks van al inicio.
+    // Se reinicia el estado (rerender).
+    const [count, setCount] = useState(quantity)
+
+    const handleAdd = () => {
+        setCount(count + 1);
+    }
+    const handleSubtract = () => {
+        if (count === 0) return;
+        setCount(count - 1);
+    }
+
     return (
-        <section style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: 10,
-            margin: 10,
-        }}>
+        <section className='item-row'>
             <span
+                className="item-text"
                 style={{
-                    width: 150
+                    color: count === 0 ? 'red' : 'black',
                 }}
             >{name}</span>
-
-            <button>+1</button>
-            <span>{quantity}</span>
-            <button>-1</button>
-        </section>
+            <button onClick={handleAdd}>+1</button>
+            <span>{count}</span>
+            <button onClick={handleSubtract}>-1</button>
+        </section >
     );
 };

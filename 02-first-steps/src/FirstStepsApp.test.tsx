73ee161fccd9bfe3bb932a549e52acce9981b12c
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-11 20:00:43 -0600

    Esperar argumentos específicos.

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,24 +1,57 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
 
+const mockItemCounter = vi.fn((_props: unknown) => {
+    return <div data-testid="item-counter" />;
+});
+
 vi.mock('./shopping-cart/itemCounter', () => ({
-    ItemCounter: () => <div data-testid="item-counter" />
+    ItemCounter: (props: unknown) => mockItemCounter(props),
 }));
 
+// vi.mock('./shopping-cart/itemCounter', () => ({
+//     ItemCounter: (props: unknown) =>
+//         <div
+//             data-testid="item-counter"
+//             name={props.name}
+//             quantity={props.quantity}
+//         />
+// }));
+
 describe('FirstSteppApp', () => {
+
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
     test('Should match snapshot', () => {
         const { container } = render(<FirstStepsApp />)
         expect(container).toMatchSnapshot();
-
-        screen.debug();
     });
 
     test('Should render the correct number of ItemCounter components', () => {
         render(<FirstStepsApp />);
         const itemCounterElements = screen.getAllByTestId('item-counter');
-
         expect(itemCounterElements.length).toBe(3);
-        screen.debug();
+    })
+
+    test('Should render ItemCounter with correct props', () => {
+        render(<FirstStepsApp />);
+
+        expect(mockItemCounter).toHaveBeenCalledTimes(3);
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Nintendo Switch',
+            quantity: 4,
+        });
+
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Pro controller',
+            quantity: 3,
+        });
+        expect(mockItemCounter).toHaveBeenCalledWith({
+            name: 'Xbox',
+            quantity: 5,
+        });                
     })
 });

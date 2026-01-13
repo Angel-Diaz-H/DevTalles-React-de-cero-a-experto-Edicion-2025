2427e91635afb62193fbb829f8d55e2c2939c904
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-01-13 17:06:10 -0600

    Prettier activo - Retorno de curso.

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -1,23 +1,26 @@
-import { mockGifs } from "./mock-data/gifs.mock"
-import { CustomHeader } from "./shared/components/CustomHeader"
-import { SearchBar } from './shared/components/SearchBar';
-import { PreviousSearches } from './gifs/PreviousSearches';
-import { GifList } from './gifs/GifList';
+import { mockGifs } from "./mock-data/gifs.mock";
+import { CustomHeader } from "./shared/components/CustomHeader";
+import { SearchBar } from "./shared/components/SearchBar";
+import { PreviousSearches } from "./gifs/PreviousSearches";
+import { GifList } from "./gifs/GifList";
 
 export const GifsApp = () => {
-    return (
-        <>
-            {/* Header */}
-            <CustomHeader title="Buscador de GIFs" description="¡Descubre y comparte el GIF perfecto!" />
+  return (
+    <>
+      {/* Header */}
+      <CustomHeader
+        title="Buscador de GIFs"
+        description="¡Descubre y comparte el GIF perfecto!"
+      />
 
-            {/* Search */}
-            <SearchBar placeholder="Busca lo que quieras!" />
+      {/* Search */}
+      <SearchBar placeholder="Busca lo que quieras!" />
 
-            {/* Búsquedas previas */}
-            <PreviousSearches searches={['Gokú', 'Dragon Ball Súper']} />
+      {/* Búsquedas previas */}
+      <PreviousSearches searches={["Gokú", "Dragon Ball Súper"]} />
 
-            {/* Gifs */}
-            <GifList gifs={mockGifs} />
-        </>
-    )
-}
+      {/* Gifs */}
+      <GifList gifs={mockGifs} />
+    </>
+  );
+};

35ce98e4d40d08cb1c1c009abf6e79f8219c77b6
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-01-28 16:50:03 -0600

    Axios mock adapter - Controlar resultados de Axios

--- 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.test.ts
+++ 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,21 +1,41 @@
-import { describe, expect, test } from "vitest";
+import { describe, test, expect } from "vitest";
+import AxiosMockAdapter from "axios-mock-adapter";
+
 import { getGifsByQuery } from "./get-gifs-by-query.action";
+import { giphyApi } from "../api/giphy.api";
+import { giphySearchResponseMock } from "../../../tests/mock/giphy.response.data";
 
 describe("getGifsByQuery", () => {
+  const mock = new AxiosMockAdapter(giphyApi);
+
+  // test("should return a list of gifs", async () => {
+  //   const gifs = await getGifsByQuery("Goku");
+  //   const [gif1] = gifs;
+
+  //   // console.log(gifs);
+
+  //   expect(gifs.length).toBe(10);
+
+  //   expect(gif1).toEqual({
+  //     id: expect.any(String),
+  //     height: expect.any(Number),
+  //     width: expect.any(Number),
+  //     title: expect.any(String),
+  //     url: expect.any(String),
+  //   });
+  // });
+
   test("should return a list of gifs", async () => {
+    mock.onGet("/search").reply(200, giphySearchResponseMock);
     const gifs = await getGifsByQuery("Goku");
-    const [gif1] = gifs;
-
-    // console.log(gifs);
-
     expect(gifs.length).toBe(10);
 
-    expect(gif1).toEqual({
-      id: expect.any(String),
-      height: expect.any(Number),
-      width: expect.any(Number),
-      title: expect.any(String),
-      url: expect.any(String),
+    gifs.forEach((gif) => {
+      expect(typeof gif.id).toBe("string");
+      expect(typeof gif.title).toBe("string");
+      expect(typeof gif.url).toBe("string");
+      expect(typeof gif.width).toBe("number");
+      expect(typeof gif.height).toBe("number");
     });
   });
 });

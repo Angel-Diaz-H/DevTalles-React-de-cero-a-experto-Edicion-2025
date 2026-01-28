c14ff9c759e5b5bc09efb6e4aa89fbc78e8fa42d
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-01-28 15:30:59 -0600

    Pruebas sobre "Custom hooks"

---
+++ 03-gifs-app/src/counter/hooks/useCounter.test.tsx
@@ -0,0 +1,46 @@
+import { describe, expect, test } from "vitest";
+import { useCounter } from "./useCounter";
+import { act, renderHook } from "@testing-library/react";
+
+describe("useCounter", () => {
+  test("should initialiaze with default value of 5", () => {
+    const { result } = renderHook(() => useCounter());
+    expect(result.current.counter).toBe(5);
+  });
+
+  test("should initialiaze with value of 20", () => {
+    const initialValue = 20;
+    const { result } = renderHook(() => useCounter(initialValue));
+    expect(result.current.counter).toBe(20);
+  });
+
+  test("should increment counter when handleAdd is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.handleAdd();
+    });
+
+    expect(result.current.counter).toBe(6);
+  });
+
+  test("should decrement counter when handleSubtract is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.handleSubtract();
+    });
+
+    expect(result.current.counter).toBe(4);
+  });
+
+  test("should reset counter when handleReset is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.handleReset();
+    });
+
+    expect(result.current.counter).toBe(5);
+  });
+});

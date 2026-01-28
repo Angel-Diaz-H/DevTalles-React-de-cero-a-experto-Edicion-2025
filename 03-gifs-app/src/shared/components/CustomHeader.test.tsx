435761ddca916bb2afc86b287ca902d1622af60a
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-01-28 15:19:31 -0600

    Pruebas sobre "CustomHeader"

---
+++ 03-gifs-app/src/shared/components/CustomHeader.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { CustomHeader } from "./CustomHeader";
+
+describe("CustomHeader", () => {
+  const title = "Test title";
+
+  test("Should render the list correctly", () => {
+    render(<CustomHeader title={title} />);
+    expect(screen.getByText(title)).toBeDefined();
+  });
+
+  test("Should render the description when provided", () => {
+    const description = "Test description";
+
+    render(<CustomHeader title={title} description={description} />);
+    // screen.debug();
+    expect(screen.getByText(description)).toBeDefined();
+    expect(screen.getByRole("paragraph")).toBeDefined();
+    expect(screen.getByRole("paragraph").innerHTML).toBe(description);
+  });
+
+  test("Should not render when not provided", () => {
+    const { container } = render(<CustomHeader title={title} />);
+    const divElement = container.querySelector(".content-center");
+    const h1 = divElement?.querySelector("h1");
+    // console.log(h1?.innerHTML);
+    expect(h1?.innerHTML).toBe(title);
+
+    const p = divElement?.querySelector("p");
+    expect(p).toBeNull();
+  });
+});

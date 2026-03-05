3d1f58db95b0fe29ce92878cbcfe4953e7713afa
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-03-05 16:18:00 -0600

    Pruebas sobre useGifs - Caché

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -7,16 +7,17 @@ import type { Gif } from "../interfaces/gif.interface";
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-  const gifsCache = useRef<Record<string, Gif[]>>({}).current;
+  const gifsCache = useRef<Record<string, Gif[]>>({});
 
   const handleTermClicked = async (term: string) => {
-    if (gifsCache[term]) {
-      setGifs(gifsCache[term]);
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
       return;
     }
 
     const gifs = await getGifsByQuery(term);
     setGifs(gifs);
+    gifsCache.current[term] = gifs;
   };
 
   const handleSearch = async (query: string = "") => {
@@ -24,11 +25,11 @@ export const useGifs = () => {
     if (query.length === 0) return;
     if (previousTerms.includes(query)) return;
 
-    setPreviousTerms([query, ...previousTerms].splice(0, 7));
+    setPreviousTerms([query, ...previousTerms].splice(0, 8));
     const gifs = await getGifsByQuery(query);
 
     setGifs(gifs);
-    gifsCache[query] = gifs;
+    gifsCache.current[query] = gifs;
   };
 
   return {

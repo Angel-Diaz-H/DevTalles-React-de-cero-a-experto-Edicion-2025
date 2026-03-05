8c0b530ccf369571a8deab645ec40b203a8cc1c3
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-03-05 15:24:41 -0600

    Excepciones en las peticiones

--- 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.test.ts
+++ 03-gifs-app/src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 import AxiosMockAdapter from "axios-mock-adapter";
 
 import { getGifsByQuery } from "./get-gifs-by-query.action";
@@ -6,7 +6,12 @@ import { giphyApi } from "../api/giphy.api";
 import { giphySearchResponseMock } from "../../../tests/mock/giphy.response.data";
 
 describe("getGifsByQuery", () => {
-  const mock = new AxiosMockAdapter(giphyApi);
+  let mock = new AxiosMockAdapter(giphyApi);
+
+  beforeEach(() => {
+    // mock.reset();
+    mock = new AxiosMockAdapter(giphyApi);
+  });
 
   // test("should return a list of gifs", async () => {
   //   const gifs = await getGifsByQuery("Goku");
@@ -37,5 +42,28 @@ describe("getGifsByQuery", () => {
       expect(typeof gif.width).toBe("number");
       expect(typeof gif.height).toBe("number");
     });
+  });
+
+  test("should return an empty list of gifs if query is empty", async () => {
+    // mock.onGet("/search").reply(200, { data: [] });
+
+    mock.restore();
+    const gifs = await getGifsByQuery("");
+
+    console.log(gifs);
+
+    expect(gifs.length).toBe(0);
+  });
+
+  test("should handle error when the API returns an error", async () => {
+    mock.onGet("/search").reply(400, {
+      data: {
+        message: "Bad request",
+      },
+    });
+
+    const gifs = await getGifsByQuery("Goku");
+    console.log(gifs);
+    expect(gifs.length).toBe(0);
   });
 });

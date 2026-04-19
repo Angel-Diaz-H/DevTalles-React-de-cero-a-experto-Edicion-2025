fb5f233c915d3264e727f5dcc07a6ef696c7d91b
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-04-18 19:42:38 -0600

    Continuación de proyecto - HooksApp

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,14 +1,15 @@
-import { StrictMode, Suspense } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'sonner';
 import './index.css';
-import { ClientInformation } from './08-use-suspense/ClientInformation';
-import { getUserAction } from './08-use-suspense/api/get-user.action';
+import { ProfessionalApp } from './09-useContext/ProfessionalApp';
+// import { ClientInformation } from './08-use-suspense/ClientInformation';
+// import { getUserAction } from './08-use-suspense/api/get-user.action';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster />
-    <Suspense
+    {/* <Suspense
       fallback={
         <div className="bg-gradient flex flex-col">
           <h1 className="text-2xl">Cargando</h1>
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
       }
     >
       <ClientInformation getUser={getUserAction(100)} />
-    </Suspense>
+    </Suspense> */}
+    <ProfessionalApp />
   </StrictMode>,
 );

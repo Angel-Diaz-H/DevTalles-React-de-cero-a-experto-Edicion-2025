93003ff6c105f8fa59d7fc3de067e342024e3f48
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-04-18 19:59:06 -0600

    React Router - Manejo de rutas.

--- 04-hooks-app/src/09-useContext/ProfessionalApp.tsx
+++ 04-hooks-app/src/09-useContext/ProfessionalApp.tsx
@@ -1,7 +1,11 @@
+import { RouterProvider } from 'react-router';
+import { appRouter } from './router/app.router';
+
 export const ProfessionalApp = () => {
   return (
     <div className="bg-gradient flex flex-col">
       <h1>Professional App</h1>
+      <RouterProvider router={appRouter} />
     </div>
   );
 };

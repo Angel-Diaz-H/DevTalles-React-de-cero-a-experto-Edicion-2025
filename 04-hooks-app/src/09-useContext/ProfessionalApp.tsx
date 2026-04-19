5f14dddfe2d06efc059ee8c6829f355495eba76d
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-04-18 20:13:01 -0600

    Navegar entre pantallas - Diseño de páginas.

--- 04-hooks-app/src/09-useContext/ProfessionalApp.tsx
+++ 04-hooks-app/src/09-useContext/ProfessionalApp.tsx
@@ -4,7 +4,6 @@ import { appRouter } from './router/app.router';
 export const ProfessionalApp = () => {
   return (
     <div className="bg-gradient flex flex-col">
-      <h1>Professional App</h1>
       <RouterProvider router={appRouter} />
     </div>
   );

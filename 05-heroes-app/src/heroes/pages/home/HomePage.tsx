8f297c3d4194967118b7c34412c68b378e620c6e
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-04-19 20:33:49 -0600

    Componente de paginación

--- 05-heroes-app/src/heroes/pages/home/HomePage.tsx
+++ 05-heroes-app/src/heroes/pages/home/HomePage.tsx
@@ -1,9 +1,8 @@
-import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { CustomJumbotron } from "../../../components/custom/CustomJumbotron";
-import { HeroStats } from "@/heroes/components/HeroStats";
+import { CustomPagination } from "@/components/custom/CustomPagination";
 import { HeroGrid } from "../../components/HeroGrid";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useState } from "react";
 
 export const HomePage = () => {
@@ -62,30 +61,7 @@ export const HomePage = () => {
         </Tabs>
 
         {/* Pagination */}
-        <div className="flex items-center justify-center space-x-2">
-          <Button variant="outline" size="sm" disabled>
-            <ChevronLeft className="h-4 w-4" />
-            Previous
-          </Button>
-
-          <Button variant="default" size="sm">
-            1
-          </Button>
-          <Button variant="outline" size="sm">
-            2
-          </Button>
-          <Button variant="outline" size="sm">
-            3
-          </Button>
-          <Button variant="ghost" size="sm" disabled>
-            <MoreHorizontal className="h-4 w-4" />
-          </Button>
-
-          <Button variant="outline" size="sm">
-            Next
-            <ChevronRight className="h-4 w-4" />
-          </Button>
-        </div>
+        <CustomPagination totalPages={5} />
       </>
     </>
   );

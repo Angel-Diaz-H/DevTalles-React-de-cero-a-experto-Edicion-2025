5eb9820fc94dc51022c4397cecfd178926884a17
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-05-12 15:49:46 -0600

    Buscar héroes por nombre

--- 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,16 @@ import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
 import { SearchControls } from "./ui/SearchControls";
 import { CustomBreacrumbs } from "@/components/custom/CustomBreacrumbs";
+import { HeroGrid } from "../../components/HeroGrid";
+import { useQuery } from "@tanstack/react-query";
+import { getHeroAction } from "@/heroes/actions/get-hero.action";
 
 export const SearchPage = () => {
+  const { data: superheroData, isError } = useQuery({
+    queryKey: ["hero", idSlug],
+    queryFn: () => getHeroAction(idSlug),
+    retry: false,
+  });
   return (
     <>
       <CustomJumbotron
@@ -23,6 +31,8 @@ export const SearchPage = () => {
       <HeroStats />
 
       <SearchControls />
+
+      <HeroGrid heroes={[]} />
     </>
   );
 };

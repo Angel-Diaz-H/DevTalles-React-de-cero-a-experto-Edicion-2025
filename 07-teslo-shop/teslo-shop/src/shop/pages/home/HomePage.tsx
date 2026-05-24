8a0fc02053b72672fefbcaa5d12591187b156f95
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-05-24 01:05:31 -0600

    Tailwind + ShadCN UI + Google Fonts

--- 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <h1 className="font-montserrat font-thin">Hola mundo</h1>
+      <Button>Hello world</Button>
+    </div>
+  );
 };

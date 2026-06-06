43c3ad36da54858e4a531282877cc105fd97f47c
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-06-05 23:33:47 -0600

    Filtros por género y tallas

--- 07-teslo-shop/teslo-shop/src/hooks/useProducts.tsx
+++ 07-teslo-shop/teslo-shop/src/hooks/useProducts.tsx
@@ -1,11 +1,26 @@
 import { getProductsAction } from "@/shop/actions/get-products.action";
 import { useQuery } from "@tanstack/react-query";
+import { useParams, useSearchParams } from "react-router";
 
 export const useProducts = () => {
-  // TODO: Lógica.
+  const { gender } = useParams();
+
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get("page") || 1;
+  const limit = searchParams.get("limit") || 9;
+  const sizes = searchParams.get("sizes") || undefined;
+
+  const offset = (Number(page) - 1) * Number(limit);
 
   return useQuery({
-    queryKey: ["products"],
-    queryFn: getProductsAction,
+    queryKey: ["products", { limit, offset, gender, sizes }],
+    queryFn: () =>
+      getProductsAction({
+        limit: Number(+limit) ? 9 : limit,
+        offset: isNaN(offset) ? 0 : offset,
+        gender,
+        sizes,
+      }),
+    staleTime: 1000 * 60 * 5, // 5 minutos
   });
 };

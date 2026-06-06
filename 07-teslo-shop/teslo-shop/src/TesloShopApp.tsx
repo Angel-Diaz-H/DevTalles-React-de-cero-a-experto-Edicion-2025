c982226934bfa25e214206e3350dc35f147c3eeb
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-06-05 21:31:56 -0600

    TanStackk Query - DevTools y Axios

--- 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
+++ 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
@@ -1,10 +1,16 @@
 import { RouterProvider } from "react-router";
 import { appRouter } from "./app.router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export const TesloShopApp = () => {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
       <RouterProvider router={appRouter} />
-    </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };

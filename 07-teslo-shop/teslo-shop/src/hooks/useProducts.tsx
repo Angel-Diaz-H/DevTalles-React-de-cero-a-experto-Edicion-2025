0486df125bb5a1fa84a39dfeff968868714ea955
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-06-05 23:45:24 -0600

    Filtrar por rango de precios

--- 07-teslo-shop/teslo-shop/src/hooks/useProducts.tsx
+++ 07-teslo-shop/teslo-shop/src/hooks/useProducts.tsx
@@ -12,14 +12,43 @@ export const useProducts = () => {
 
   const offset = (Number(page) - 1) * Number(limit);
 
+  const price = searchParams.get("price") || "any";
+  let minPrice = undefined;
+  let maxPrice = undefined;
+
+  switch (price) {
+    case "any":
+      break;
+
+    case "0-50":
+      minPrice = 0;
+      maxPrice = 50;
+      break;
+
+    case "50-100":
+      minPrice = 50;
+      maxPrice = 100;
+      break;
+
+    case "200+":
+      minPrice = 200;
+      maxPrice = undefined;
+      break;
+  }
+
   return useQuery({
-    queryKey: ["products", { limit, offset, gender, sizes }],
+    queryKey: [
+      "products",
+      { limit, offset, gender, sizes, price, minPrice, maxPrice },
+    ],
     queryFn: () =>
       getProductsAction({
         limit: Number(+limit) ? 9 : limit,
         offset: isNaN(offset) ? 0 : offset,
         gender,
         sizes,
+        minPrice,
+        maxPrice,
       }),
     staleTime: 1000 * 60 * 5, // 5 minutos
   });

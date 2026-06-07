2f60514ebf5dd5bc0c87f6171fd0d0611fec4db2
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-06-06 18:41:56 -0600

    Acción de inicio de sesión

--- 07-teslo-shop/teslo-shop/src/auth/layouts/pages/login/LoginPage.tsx
+++ 07-teslo-shop/teslo-shop/src/auth/layouts/pages/login/LoginPage.tsx
@@ -4,13 +4,24 @@ import { CustomLogo } from "../../../../components/Custom/CustomLogo";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router";
+import type { FormEvent } from "react";
 
 export const LoginPage = () => {
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    // Evita un full refresh del navegador al enviar el formulario.
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    // Extrae los valores de email y password del formulario.
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+  };
   return (
     <div className={"flex flex-col gap-6"}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleLogin}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <CustomLogo subtitle="Login" />
@@ -23,6 +34,7 @@ export const LoginPage = () => {
                 <Input
                   id="email"
                   type="email"
+                  name="email"
                   placeholder="mail@example.com"
                   required
                 />
@@ -40,6 +52,7 @@ export const LoginPage = () => {
                 <Input
                   id="password"
                   type="password"
+                  name="password"
                   required
                   placeholder="Contraseña segura"
                 />

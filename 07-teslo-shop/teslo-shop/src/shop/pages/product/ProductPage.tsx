b3a5a666683184509da86f08b36443a82d4349af
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2026-06-12 10:44:21 -0600

    Gestor de estado - Zustand

--- 07-teslo-shop/teslo-shop/src/shop/pages/product/ProductPage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/product/ProductPage.tsx
@@ -1,3 +1,14 @@
+import { useCounterStore } from "@/auth/store/auth.store";
+import { Button } from "@/components/ui/button";
+
 export const ProductPage = () => {
-  return <div>ProductPage</div>;
+  const { count, inc, dec } = useCounterStore();
+
+  return (
+    <>
+      <h1 className="text-3xl font-monsterrat">Count: {count}</h1>
+      <Button onClick={dec}>-1</Button>
+      <Button onClick={inc}>+1</Button>
+    </>
+  );
 };
